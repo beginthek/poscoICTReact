5a37bde77940d84e9e5de7abefca6c0f32ca5e3e
sungyun kim <yunkim1992@gmail.com>
2022-05-31 11:35:03 +0900

    bootstrap login

--- study/src/App.js
+++ study/src/App.js
@@ -1,33 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from "react";
-import TodoList from "./component/TodoList";
-import SaveTodo from './component/SaveTodo';
+import logo from "./logo.svg"
+import "./App.css"
+import { useState } from "react"
+import TodoList from "./component/TodoList"
+import SaveTodo from "./component/SaveTodo"
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
+    const [
+        todoList,
+        setTodoList,
+    ] =
+        useState(
+            []
+        )
 
-  const saveTodo = (e) => {
-    let todo = document.getElementById("todo");
-    let copy = [...todoList];
-    copy.push(todo.value);
-    setTodoList(copy);
-    todo.value = '';
-    //
-  };
+    const saveTodo =
+        (
+            e
+        ) => {
+            let todo =
+                document.getElementById(
+                    "todo"
+                )
+            let copy =
+                [
+                    ...todoList,
+                ]
+            copy.push(
+                todo.value
+            )
+            setTodoList(
+                copy
+            )
+            todo.value =
+                ""
+            //
+        }
 
-  const deleteTodo = (e) => {
-    let parentElement = e.target.parentElement;
-    parentElement.style.textDecorationLine = "line-through";
-  };
-  
-  return (
-    <div className="App">
-      <h1>Todo List</h1>
-      <SaveTodo saveTodo={saveTodo}/>
-      <TodoList todoList={todoList} deleteTodo={deleteTodo}/>
-    </div>
-  );
+    const deleteTodo =
+        (
+            e
+        ) => {
+            let parentElement =
+                e
+                    .target
+                    .parentElement
+            parentElement.style.textDecorationLine =
+                "line-through"
+        }
+    ;<h1></h1>
+    return (
+        <div className="App">
+            <h1>
+                Todo
+                List
+            </h1>
+
+            <SaveTodo
+                saveTodo={
+                    saveTodo
+                }
+            />
+            <TodoList
+                todoList={
+                    todoList
+                }
+                deleteTodo={
+                    deleteTodo
+                }
+            />
+        </div>
+    )
 }
 
-export default App;
+export default App
